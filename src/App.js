899be816f575dbd86b9c7cf129d9a7154d94137a
gurpreetengineer <gurpreet.reactdev@gmail.com>
2023-04-12 10:26:16 +0530

    other files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+const initialColumns = [
+  { id: 1, title: 'To Do', tasks: ['Task 1', 'Task 2'] },
+  { id: 2, title: 'In Progress', tasks: ['Task 3'] },
+  { id: 3, title: 'Done', tasks: ['Task 4', 'Task 5'] },
+];
 
 function App() {
+  const [columns, setColumns] = useState(initialColumns);
+
+  function handleDragStart(event, taskIndex, columnIndex) {
+    event.dataTransfer.setData('taskIndex', taskIndex);
+    event.dataTransfer.setData('columnIndex', columnIndex);
+  }
+
+  function handleDragOver(event) {
+    event.preventDefault();
+  }
+
+  function handleDrop(event, targetColumnIndex) {
+    const sourceTaskIndex = event.dataTransfer.getData('taskIndex');
+    const sourceColumnIndex = event.dataTransfer.getData('columnIndex');
+
+    const newColumns = [...columns];
+
+    // Remove task from source column
+    const sourceColumn = newColumns[sourceColumnIndex];
+    const task = sourceColumn.tasks.splice(sourceTaskIndex, 1)[0];
+
+    // Add task to target column
+    const targetColumn = newColumns[targetColumnIndex];
+    targetColumn.tasks.push(task);
+
+    setColumns(newColumns);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="kanban-board">
+      {columns.map((column, columnIndex) => (
+        <div
+          key={column.id}
+          className="kanban-column"
+          onDragOver={handleDragOver}
+          onDrop={(event) => handleDrop(event, columnIndex)}
         >
-          Learn React
-        </a>
-      </header>
+          <h2>{column.title}</h2>
+          <ul>
+            {column.tasks.map((task, taskIndex) => (
+              <li
+                key={taskIndex}
+                draggable
+                onDragStart={(event) => handleDragStart(event, taskIndex, columnIndex)}
+              >
+                {task}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
